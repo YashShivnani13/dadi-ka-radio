b52f6790384d6eb5c53b84e4d85a025d76ef8c9c
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-13 21:01:16 +0530

    Updated generatedPlaylist

--- data/generatedPlaylist.ts
+++ data/generatedPlaylist.ts
@@ -23,44 +23,98 @@ export const playlist: Song[] = [
   },
   {
     "id": "track-3",
-    "title": "Ek Din Teri Raahon",
+    "title": "Chhote Chhote Bhaiyon Ke Bade Bhaiyya - Hum Saath Saath Hain - Bollywood Wedding Song",
     "artist": "Dadi Ka Radio",
-    "audio": "/MP3/Ek%20Din%20Teri%20Raahon%20%5B55UJumsI1m0%5D.mp3"
+    "audio": "/MP3/Chhote%20Chhote%20Bhaiyon%20Ke%20Bade%20Bhaiyya%20-%20Hum%20Saath%20Saath%20Hain%20-%20Bollywood%20Wedding%20Song.mp3"
   },
   {
     "id": "track-4",
-    "title": "Haan Tu Hain",
+    "title": "Disco Station Disco - Reena Roy, Asha Bhosle, Haathkadi Song",
     "artist": "Dadi Ka Radio",
-    "audio": "/MP3/Haan%20Tu%20Hain%20%5B1kcCk4YRJdo%5D.mp3"
+    "audio": "/MP3/Disco%20Station%20Disco%20-%20Reena%20Roy%2C%20Asha%20Bhosle%2C%20Haathkadi%20Song%20%5B9fZ0UTAy75M%5D.mp3"
   },
   {
     "id": "track-5",
-    "title": "Jo Tere Sang",
+    "title": "Falguni Pathak - Maine Payal Hai Chhankai",
     "artist": "Dadi Ka Radio",
-    "audio": "/MP3/Jo%20Tere%20Sang%20%5BGeWnEyQRCUs%5D.mp3"
+    "audio": "/MP3/Falguni%20Pathak%20-%20Maine%20Payal%20Hai%20Chhankai.mp3"
   },
   {
     "id": "track-6",
-    "title": "Mann Mera (Original Version)",
+    "title": "Hare Rama Hare Krishna - I Love You",
     "artist": "Dadi Ka Radio",
-    "audio": "/MP3/Mann%20Mera%20(Original%20Version)%20%5Bs_GR_TDK07I%5D.mp3"
+    "audio": "/MP3/Hare%20Rama%20Hare%20Krishna%20-%20I%20Love%20You%20%5BSLdqaeUhu3A%5D.mp3"
   },
   {
     "id": "track-7",
+    "title": "Haye Re Haye Tera Ghungta _ Kishore Kumar, Asha Bhosle _ Dhongee Songs 1979 _ Neetu Singh",
+    "artist": "Dadi Ka Radio",
+    "audio": "/MP3/Haye%20Re%20Haye%20Tera%20Ghungta%20_%20Kishore%20Kumar%2C%20Asha%20Bhosle%20_%20Dhongee%20Songs%201979%20_%20Neetu%20Singh%20%5BH0iG8xZ6ysY%5D.mp3"
+  },
+  {
+    "id": "track-8",
+    "title": "jab chaye mera jadoo - Lootmaar 1980",
+    "artist": "Dadi Ka Radio",
+    "audio": "/MP3/jab%20chaye%20mera%20jadoo%20-%20Lootmaar%201980%20%5B6kXfs8D4-Is%5D.mp3"
+  },
+  {
+    "id": "track-9",
+    "title": "Jimmy Jimmy Ajaa Ajaa _ Disco Dancer _ Mithun Chakraborty _ Kim _ Bollywood Hit Songs",
+    "artist": "Dadi Ka Radio",
+    "audio": "/MP3/Jimmy%20Jimmy%20Ajaa%20Ajaa%20_%20Disco%20Dancer%20_%20Mithun%20Chakraborty%20_%20Kim%20_%20Bollywood%20Hit%20Songs%20%5BDKJ0jvO6fBA%5D.mp3"
+  },
+  {
+    "id": "track-10",
+    "title": "Kaanta Laga… Bangle Ke Peechhe Samadhi Songs Asha Parekh Lata Mangeshkar Hits",
+    "artist": "Dadi Ka Radio",
+    "audio": "/MP3/Kaanta%20Laga%E2%80%A6%20Bangle%20Ke%20Peechhe%20Samadhi%20Songs%20Asha%20Parekh%20Lata%20Mangeshkar%20Hits.mp3"
+  },
+  {
+    "id": "track-11",
+    "title": "Khwaab Ho Tum Ya Koi Teen Deviyan Dev Anand Romantic Old Hindi Songs Kishore Kumar",
+    "artist": "Dadi Ka Radio",
+    "audio": "/MP3/Khwaab%20Ho%20Tum%20Ya%20Koi%20Teen%20Deviyan%20Dev%20Anand%20Romantic%20Old%20Hindi%20Songs%20Kishore%20Kumar.mp3"
+  },
+  {
+    "id": "track-12",
     "title": "MERE RANG ME RANG NE WALI✨ [lyrics]",
     "artist": "Dadi Ka Radio",
     "audio": "/MP3/MERE%20RANG%20ME%20RANG%20NE%20WALI%E2%9C%A8%20%5Blyrics%5D%20%5Bd1wzzmwv3uM%5D.mp3"
   },
   {
-    "id": "track-8",
+    "id": "track-13",
     "title": "Neele Neele Ambar Par (Lyrics) - Kishore Kumar, Kalyanji-Anandji",
     "artist": "Dadi Ka Radio",
     "audio": "/MP3/Neele%20Neele%20Ambar%20Par%20(Lyrics)%20-%20Kishore%20Kumar%2C%20Kalyanji-Anandji.mp3"
   },
   {
-    "id": "track-9",
+    "id": "track-14",
+    "title": "O Miss De De Kiss - Govinda, Rohan Kapoor, Love 86 Song (k)",
+    "artist": "Dadi Ka Radio",
+    "audio": "/MP3/O%20Miss%20De%20De%20Kiss%20-%20Govinda%2C%20Rohan%20Kapoor%2C%20Love%2086%20Song%20(k)%20%5BzYDwEHZ0TYA%5D.mp3"
+  },
+  {
+    "id": "track-15",
     "title": "PYAR KIYA TO NIBHANA",
     "artist": "Dadi Ka Radio",
     "audio": "/MP3/PYAR%20KIYA%20TO%20NIBHANA%20%5BXuA0BvYksyI%5D.mp3"
+  },
+  {
+    "id": "track-16",
+    "title": "Sara Zamana Haseeno Ka Deewana Kishore Kumar Yaarana 1981 Songs Amitabh Bachchan",
+    "artist": "Dadi Ka Radio",
+    "audio": "/MP3/Sara%20Zamana%20Haseeno%20Ka%20Deewana%20Kishore%20Kumar%20Yaarana%201981%20Songs%20Amitabh%20Bachchan.mp3"
+  },
+  {
+    "id": "track-17",
+    "title": "Tu Pyar Hai Kisi Aur Ka (Full Song) Aamir K, Pooja B Anuradha P, Kumar Sanu Dil Hai Ke Manta Nahin",
+    "artist": "Dadi Ka Radio",
+    "audio": "/MP3/Tu%20Pyar%20Hai%20Kisi%20Aur%20Ka%20(Full%20Song)%20Aamir%20K%2C%20Pooja%20B%20Anuradha%20P%2C%20Kumar%20Sanu%20Dil%20Hai%20Ke%20Manta%20Nahin.mp3"
+  },
+  {
+    "id": "track-18",
+    "title": "Zooby Zooby Lyrical Video Dance Dance Alisha Chinoy Bappi Lahiri Mithun, Smita Patil",
+    "artist": "Dadi Ka Radio",
+    "audio": "/MP3/Zooby%20Zooby%20Lyrical%20Video%20Dance%20Dance%20Alisha%20Chinoy%20Bappi%20Lahiri%20Mithun%2C%20Smita%20Patil.mp3"
   }
 ];
